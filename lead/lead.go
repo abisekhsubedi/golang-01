afabd2cdcebb4ce9113188879380a89e93c6c76e
abisekhsubedi <abisekh.work@gmail.com>
2022-06-21 00:15:59 +0545

    init: structure go project dir

---
+++ lead/lead.go
@@ -0,0 +1,50 @@
+package lead
+
+import (
+	"github.com/abisekhsubedi/golang-01/database"
+	"github.com/gofiber/fiber"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
+)
+
+// struct: A datatype I am creating on my own
+type Lead struct {
+	gorm.Model
+	Name    string
+	Company string
+	Email   string
+	Phone   int
+}
+
+func GetLeads(c *fiber.Ctx) {
+	db := database.DBConn
+	var leads []Lead
+	db.Find(&leads)
+	c.JSON(leads)
+}
+
+func Getlead(c *fiber.Ctx) {
+	id := c.Params("id")
+	db := database.DBConn
+	var lead Lead
+	db.Find(&lead, id)
+	c.JSON(lead)
+
+}
+
+func NewLead(c *fiber.Ctx) {
+	db := database.DBConn
+	lead := new(Lead)
+	if err := c.BodyParser(lead); err != nil {
+		c.Status(503).SendString("Error parsing body")
+		return
+	}
+
+	db.Create(&lead)
+	c.JSON(lead)
+
+}
+
+func DeleteLead() {
+	
+}
--- main.go
+++ main.go
@@ -1,30 +1,42 @@
 package main
 
 import (
-	"github.com/gofiber/fiber"
+	"fmt"
+
 	"github.com/abisekhsubedi/golang-01/database"
+	"github.com/abisekhsubedi/golang-01/lead"
+	"github.com/gofiber/fiber"
+	"github.com/jinzhu/gorm"
 )
 
-func setupRoute(app *fiber.App){
-	app.Get(GetLeads)
-	app.Get(GetLeads)
-	app.post(NewLead)
-	app.Post(NewLead)
-	app.Delete(DeleteLead)
+func setupRoute(app *fiber.App) {
+	app.Get("/api/v1/lead", lead.GetLeads)
+	app.Get("/api/v1/lead/:id", lead.Getlead)
+	app.Post("/api/v1/lead", lead.NewLead)
+	app.Delete("/api/v1/lead/:id", lead.DeleteLead)
 }
 
-func initDatabase(){
+func initDatabase() {
+	var err error
 
+	database.DBConn, err = gorm.Open("sqlite3", "leads.db")
+
+	if err != nil {
+		panic("failed to connect database")
+	}
+	fmt.Println("Connection opened to database")
+	database.DBConn.AutoMigrate(&lead.Lead{})
+	fmt.Println("Database Migrated")
 }
 
 func main() {
 	app := fiber.New()
+
 	initDatabase()
+
 	setupRoute(app)
 
 	// listen to port 3000
 	app.Listen(3000)
-
 	defer database.DBConn.Close()
-
 }
